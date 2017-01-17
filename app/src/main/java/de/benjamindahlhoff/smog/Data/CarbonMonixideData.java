daf6ba8eaa16fcf4306404b8a1ce68cbafd5a562
Benjamin Dahlhoff <benjamin@dahlhoff.com>
2017-01-17 11:11:33 +0100

    Created Class CarbonMonoxideData. Compare Values to: http://openweathermap.org/api/pollution/co

---
+++ app/src/main/java/de/benjamindahlhoff/smog/Data/CarbonMonixideData.java
@@ -0,0 +1,41 @@
+package de.benjamindahlhoff.smog.Data;
+
+/**
+ * Created by Benjamin Dahlhoff on 17.01.17.
+ */
+
+public class CarbonMonixideData {
+    private double mCarbonMonoxideVolumeMixingRatio;
+    private double mAtmosphericPressure;
+    private double mMeasurementPrecision;
+
+    public CarbonMonixideData(double carbonMonoxideVolumeMixingRatio, double atmosphericPressure, double measurementPrecision) {
+        mCarbonMonoxideVolumeMixingRatio = carbonMonoxideVolumeMixingRatio;
+        mAtmosphericPressure = atmosphericPressure;
+        mMeasurementPrecision = measurementPrecision;
+    }
+
+    public double getCarbonMonoxideVolumeMixingRatio() {
+        return mCarbonMonoxideVolumeMixingRatio;
+    }
+
+    public void setCarbonMonoxideVolumeMixingRatio(double carbonMonoxideVolumeMixingRatio) {
+        mCarbonMonoxideVolumeMixingRatio = carbonMonoxideVolumeMixingRatio;
+    }
+
+    public double getAtmosphericPressure() {
+        return mAtmosphericPressure;
+    }
+
+    public void setAtmosphericPressure(double atmosphericPressure) {
+        mAtmosphericPressure = atmosphericPressure;
+    }
+
+    public double getMeasurementPrecision() {
+        return mMeasurementPrecision;
+    }
+
+    public void setMeasurementPrecision(double measurementPrecision) {
+        mMeasurementPrecision = measurementPrecision;
+    }
+}
