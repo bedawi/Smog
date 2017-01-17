7073b02f62e76e2c3b7c3c46edf37ada5df04f9b
Benjamin Dahlhoff <benjamin@dahlhoff.com>
2017-01-17 11:21:43 +0100

    Minor work on MainActivity

--- app/src/main/java/de/benjamindahlhoff/smog/UI/MainActivity.java
+++ app/src/main/java/de/benjamindahlhoff/smog/UI/MainActivity.java
@@ -3,9 +3,13 @@ package de.benjamindahlhoff.smog.UI;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import de.benjamindahlhoff.smog.Data.Pollution;
 import de.benjamindahlhoff.smog.R;
 
 public class MainActivity extends AppCompatActivity {
+    public final static String TAG = MainActivity.class.getSimpleName();
+
+    private Pollution mPollution;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
